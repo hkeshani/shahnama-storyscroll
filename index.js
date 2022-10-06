5275994e025fc8ac25181e5678b95ff7b0ae1309
hkeshani <ubco.arth.keshani@gmail.com>
2022-10-05 21:17:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@
     var rc = new L.RasterCoords(map, img)
 
     // set the view on a marker ...
-    //map.setView(rc.unproject([4900, 3015]), 1)
+    map.setView(rc.unproject([4900, 3015]), 1)
 /*
     // add layer control object
     L.control.layers({}, {
