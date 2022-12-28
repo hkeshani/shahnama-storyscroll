b8c05e55713a47589da97daf4a0eb8a9d44b19c1
hkeshani <ubco.arth.keshani@gmail.com>
2022-12-27 19:15:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
   });
  
   $('div#contents').scroll(function() {    
-            $('.title-text').css({'opacity':(( 1000-scrollPosition )/1000)});
+            $('.title-text').css({'opacity':(( 100-scrollPosition )/100)});
         });
   
   function init (mapid) {
